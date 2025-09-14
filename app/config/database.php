306e73d784dacdcb66579a470fbe28472f28d5f2
markneilfabellon <markneilfabellon25@gmail.com>
2025-09-14 15:58:57 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,15 +59,17 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
-    'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'hostname'	=> getenv('DB_HOST'),
+    'port'		=> getenv('DB_PORT'),
+    'username'	=> getenv('DB_USER'),
+    'password'	=> getenv('DB_PASS'),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
     'path'      => ''
 );
+
+
+
 
 ?>
